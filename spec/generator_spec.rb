eb5ab3979a6ac88da52c6cd3bf6d355ed3d62d91
BlueVajra <cory.leistikow@gmail.com>
2014-06-04 09:22:41 -0600

    randomness generator returns proper number of companies

--- spec/generator_spec.rb
+++ spec/generator_spec.rb
@@ -1,1 +1,19 @@
-# TODO: um, yeah... I didn't have time to write the spec, sorry...
+require 'generator'
+require 'pp'
+
+describe Generator do
+  it "it generates 1 company for every 2 people" do
+    number_of_people = 4
+    people = Generator.new(number_of_people).generate
+    expected = number_of_people/2
+    actual = people.map { |person| person[2]}.uniq.length
+    expect(actual).to eq expected
+  end
+  it "it generates 1 company for every 2 people, greater number" do
+    number_of_people = 50
+    people = Generator.new(number_of_people).generate
+    expected = number_of_people/2
+    actual = people.map { |person| person[2]}.uniq.length
+    expect(actual).to eq expected
+  end
+end
